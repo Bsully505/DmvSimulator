8483dd9be288f449276f88eabffa6db36f3ea265
Bryan Sullivan <Bsully505@yahoo.com>
2021-03-01 21:28:52 -0500

    added more tickets

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -2,10 +2,10 @@ public class DmvSimulator {
 
     public static void main(String[] args){
         System.out.println("Welcome to the DMV please wait until give a ticket number");
-        int ticketNum = (int)(Math.random()*100);//random num 1-100
+        int ticketNum = (int)(Math.random()*200);//random num 1-100
         System.out.format("your Ticket is %d \n", ticketNum);
-        for (int i = 0 ; i< 100 ;i++){
-            System.out.format("Now calling Ticket %d \n", ((i+ticketNum)%100)+1);
+        for (int i = 0 ; i< 200 ;i++){
+            System.out.format("Now calling Ticket %d \n", ((i+ticketNum)%200)+1);
         }
         System.out.println("You do not have the required papers you have to go get those and come back. NEXT!");
 
